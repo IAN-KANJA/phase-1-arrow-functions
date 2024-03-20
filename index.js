e8b9277d4192e3dfad3a6fbf61f328ea7ed3df8c
iannjau <lantanacreations@gmail.com>
2024-03-20 15:37:24 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+function divide (parameter1 ,parameter2) {
+    console.log(`divide ${parameter1}`);
+    console.log(`divide ${parameter2}`);
+    return parameter1 / parameter2;
+    
+}
+divide(2000/100);
+
+
+const square = () => {
+    let num1 = 2
+    return num1 ** 2;
+}
+const add =(parameter1,parameter2) =>{
+    console.log(`Adding ${parameter1}`);
+  console.log(`Adding ${parameter2}`);
+  return parameter1 + parameter2; 
+
+}
+sum (3,4);
